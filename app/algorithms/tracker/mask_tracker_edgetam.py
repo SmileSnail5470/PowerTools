b89addccb2d08e36320036ba4ad91099e8241eba
SmailSnail5470 <yangliuqing008@realsee.com>
2026-07-14 19:28:07 +0800

    新的跟踪算法支持ioboinding

--- app/algorithms/tracker/mask_tracker_edgetam.py
+++ app/algorithms/tracker/mask_tracker_edgetam.py
@@ -35,6 +35,7 @@ class MaskTrackerEdgeTAM:
         self.memory_attention = _load("memory_attention")
         self.image_decoder = _load("image_decoder")
         self.mem_encoder = _load("mem_encoder")
+        self._use_iobinding = self.image_encoder.use_cuda
         self.tpos_enc = np.load(os.path.join(model_dir, "maskmem_tpos_enc.npy"))
         with open(os.path.join(model_dir, "meta.json")) as f:
             self.meta = json.load(f)
@@ -87,6 +88,33 @@ class MaskTrackerEdgeTAM:
         tok = obj_ptrs.reshape(num, 1, 4, self.mem_dim).transpose(0, 2, 1, 3).reshape(4 * num, 1, self.mem_dim)
         return spatial_memory, spatial_memory_pos, tok.astype(np.float32)
 
+    def _enc_image(self, image):
+        if self._use_iobinding:
+            return self.image_encoder.run_with_iobinding({"image": image})
+        return self.image_encoder.run(None, {"image": image})
+
+    def _enc_mask(self, mask, pix_feat):
+        feed = {"mask": mask, "pix_feat": pix_feat}
+        if self._use_iobinding:
+            return self.mask_encoder.run_with_iobinding_numpy(feed)[0]
+        return self.mask_encoder.run(None, feed)[0]
+
+    def _enc_mem(self, high_res_mask, pix_feat):
+        feed = {"high_res_mask": high_res_mask.astype(np.float32), "pix_feat": pix_feat}
+        if self._use_iobinding:
+            return self.mem_encoder.run_with_iobinding_numpy(feed)
+        return self.mem_encoder.run(None, feed)
+
+    def _attend(self, feed):
+        if self._use_iobinding:
+            return self.memory_attention.run_with_iobinding(feed)[0]
+        return self.memory_attention.run(None, feed)[0]
+
+    def _decode(self, feed):
+        if self._use_iobinding:
+            return self.image_decoder.run_with_iobinding_numpy(feed)
+        return self.image_decoder.run(None, feed)
+
     def run(self, mask_path, frames_dir):
         frames_list = sorted([os.path.join(frames_dir, item) for item in os.listdir(frames_dir)])
         assert len(frames_list) >= 1
@@ -94,29 +122,26 @@ class MaskTrackerEdgeTAM:
         mask_out = os.path.dirname(mask_path)
         image, (H0, W0) = self._load_frame(frames_list[0])
         mask = self._load_mask(mask_path)
-        enc = self.image_encoder.run(None, {"image": image})
-        pix_feat, hr0, hr1, vfeat, vpos = enc
-        obj_ptr = self.mask_encoder.run(None, {"mask": mask, "pix_feat": pix_feat})[0]
+        pix_feat, hr0, hr1, vfeat, vpos = self._enc_image(image)
+        obj_ptr = self._enc_mask(mask, pix_feat)
         high_res = mask * 20.0 - 10.0
-        mm, mmpos = self.mem_encoder.run(None, {"high_res_mask": high_res.astype(np.float32), "pix_feat": pix_feat})
+        mm, mmpos = self._enc_mem(high_res, pix_feat)
         bank[0] = {"mm": mm, "mmpos": mmpos, "obj_ptr": obj_ptr, "cond": True}
         Image.fromarray((self._mask_to_orig(mask[0, 0], (H0, W0)) * 255).astype(np.uint8)).save(os.path.join(mask_out, f"{os.path.splitext(frames_list[0])[0]}.png"))
         
         for frame_idx in range(1, len(frames_list)):
             image, (H, W) = self._load_frame(frames_list[frame_idx])
-            enc = self.image_encoder.run(None, {"image": image})
-            pix_feat, hr0, hr1, vfeat, vpos = enc
+            pix_feat, hr0, hr1, vfeat, vpos = self._enc_image(image)
             sp_mem, sp_pos, tok = self._build_memory(frame_idx, bank)
-            image_embed = self.memory_attention.run(None, {
+            image_embed = self._attend({
                 "vision_feat": vfeat, "vision_pos_embed": vpos,
                 "spatial_memory": sp_mem, "spatial_memory_pos": sp_pos,
                 "obj_ptr_tokens": tok,
-            })[0]
-
-            obj_ptr, high_res = self.image_decoder.run(None, {
+            })
+            obj_ptr, high_res = self._decode({
                 "image_embed": image_embed, "high_res_feat0": hr0, "high_res_feat1": hr1,
             })
-            mm, mmpos = self.mem_encoder.run(None, {"high_res_mask": high_res.astype(np.float32), "pix_feat": pix_feat})
+            mm, mmpos = self._enc_mem(high_res, pix_feat)
             bank[frame_idx] = {"mm": mm, "mmpos": mmpos, "obj_ptr": obj_ptr, "cond": False}
             logits = _resize_bilinear(high_res[0, 0], (H, W))
             tmp_mask = logits > 0
